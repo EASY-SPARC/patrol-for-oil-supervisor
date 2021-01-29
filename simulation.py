cb30c0e3b42e705df2106819cf5db28d2042104c
glauberrleite <contact@glauberrleite.com>
2021-01-29 00:29:37 -0300

    inpolygon problem

--- simulation.py
+++ simulation.py
@@ -22,6 +22,8 @@ class Simulation(object):
         self.interval   = interval
         self.is_running = False
         self.start()
+        
+        res_grid = float(res_grid)
 
         # Instance for gnome interface
         self._gnome = GnomeInterface()
@@ -36,11 +38,25 @@ class Simulation(object):
         (minLon, minLat, maxLon, maxLat) = regionPolygon.bounds
         coords = np.array(regionPolygon.exterior.coords)
 
-        # Create grid map based on region boundaries
+        # Create grid maps based on region boundaries
         self.width = np.ceil(res_grid * (maxLon - minLon))
         self.height = np.ceil(res_grid * (maxLat - minLat))
+        
+        self._mask = np.zeros((self.height, self.width))
+        self._dist_grid = np.zeros((self.height, self.width))
 
-        self._kde = np.zeros((self.height, self.width))
+        for i in range(self.width):
+            for j in range(self.height):
+                if inpolygon((i/res_grid) + minLon, (j/res_grid) + minLat, coords[:, 0], coords[:, 1]) == False:
+                    self._mask(j, i) = 1
+                else:
+                    #self._dist_grid(j, i) = res_grid * 
+                    pass
+        
+        self._kde = self._compute_kde()
+        #max_dist=max(max(self._dist_grid))
+        #self._dist_grid = 1/max_dist*5*(~self.mask.*max_dist-self._dist_grid)+self._kde
+
 
     def _run(self):
         self.is_running = False
@@ -50,12 +66,21 @@ class Simulation(object):
         [lon, lat] = self._gnome.step(datetime(2020, 9, 15, 12, 0, 0), False)
         self._kde = self._compute_kde(lon, lat)
 
-    def _compute_kde(self, lon, lat):
-        kde = np.zeros((self.height, self.width))
+    def _compute_kde(self, lon=None, lat=None):
+        kde = np.NINF * self._mask # No Fly Zones cells are -inf valued
+        kde = np.where(np.isnan(kde), 0, kde) # force numpy's -inf * 0 = 0, instead of nan
+
+        if (lon is not None) and (lat is not None):
+            # h = ...
+            # f = ...
+            #kde = 5/np.max(f) * np.logical_not(mask) * f * (h>0) + kde;
+            pass # remove
+
+
         return kde
     
     def robot_feedback(self, xgrid, ygrid, lon, lat):
-        pass
+        pass # remove
 
     def get_kde():
         return self._kde
