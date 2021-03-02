e1418cdaa9a97cb020347b369258c0cfeea2a306
glauberrleite <grl@ic.ufal.br>
2021-03-02 14:10:54 -0300

    fix on test robot feedback matlab

--- test_api_robot_fb.m
+++ test_api_robot_fb.m
@@ -2,5 +2,5 @@ import matlab.net.http.*
 import matlab.net.http.field.*
 request = RequestMessage( 'POST', ...
     [ContentTypeField( 'application/vnd.api+json' ), AcceptField('application/vnd.api+json')], ...
-    '{"xgrid": "26", "ygrid": "36", "lon": "[]", "lat": "[]"}' );
+    '{"xgrid": "21", "ygrid": "38", "lon": "[]", "lat": "[]"}' );
 response = request.send('http://localhost:5000/robot_fb/');
