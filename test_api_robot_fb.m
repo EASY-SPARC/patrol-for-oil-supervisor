755f0c1957c48598192dc23aaf1eca5a353b6302
glauberrleite <grl@ic.ufal.br>
2021-03-02 17:51:34 -0300

    moved matlab client to new repo

--- test_api_robot_fb.m
+++
@@ -1,6 +0,0 @@
-import matlab.net.http.*
-import matlab.net.http.field.*
-request = RequestMessage( 'POST', ...
-    [ContentTypeField( 'application/vnd.api+json' ), AcceptField('application/vnd.api+json')], ...
-    '{"xgrid": "21", "ygrid": "38", "lon": "[]", "lat": "[]"}' );
-response = request.send('http://localhost:5000/robot_fb/');
